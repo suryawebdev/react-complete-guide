cf42ce7e28d11a12d26ced1c88106b152bb480d6
Surya Polina <suryapweb@gmail.com>
2020-04-28 21:36:59 -0500

    Adding Person JS file and Printing random age with this commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>This is first React Application</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>This is Surya</h1>
+                <p>This is the React Application I have started working on.</p>
+                <Person />
+                <Person />
+                <Person />
+                <Person />
+            </div>
+        );
+        //return React.createElement('div', null, )
+    }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = () => {
+    return <p>I am from Person and I am {Math.floor(Math.random()*20)} years age</p>
+};
+
+export default person;
