4e53cb0893c48cc1d5eeb6cae85ca4ad1bcfc9fa
Surya Polina <suryapweb@gmail.com>
2020-09-28 22:50:44 -0500

    Prop types and Ref

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,20 +1,45 @@
 import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+
 import PersonCSS from './Person.module.css';
 import withClass from '../../../Hoc/WithClass';
 
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();//Create Ref was introduced in react 16 and later
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus()
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log("6. [Person.js] render..")
         return (
             <div className={PersonCSS.Person}>
                 <p onClick={this.props.click}>This is {this.props.name} and I am {this.props.age} years age</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                type="text" 
+                // ref={(inputEle)=> {this.inputElement = inputEle}}
+                ref={this.inputElementRef}
+                onChange={this.props.changed} 
+                value={this.props.name} 
+                />
             </div>
         )
     }
 
+};
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default withClass(Person, PersonCSS.Person);
@@ -54,3 +79,14 @@ export default withClass(Person, PersonCSS.Person);
 //         </div>
 //     )
 // };
+
+//PropTyeps:
+    //Prop types is an react extention where we can define the type of property if we want to pass the components
+    //It helps if we distribute components.
+
+//Ref:
+    //Class based component ref approach
+    //ref={(inputEle)=> {this.inputElement = inputEle}}
+    //By passing ref we can directly handle the nth element 
+
+    //React.createRef() -> creates reference to element that is assigned to and stores it
